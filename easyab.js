83d3f6593d2b055ae5804b13da9b3d49b2df479a
srom <romain.strock@gmail.com>
2013-07-01 22:21:19 +0200

    version 1.0.1

--- easyab.js
+++ easyab.js
@@ -1,10 +1,10 @@
-/* easyAB v1.0.0 | (c) 2013 @_srom | srom.github.io/easyAB | MIT @license */
+/* easyAB v1.0.1 | (c) 2013 @_srom | srom.github.io/easyAB | MIT @license */
 
 /**
  * easyAB is a light-weight jQuery / Zepto plugin for quickly and easily setting up A/B tests.
  * @see srom.github.io/easyAB
  * @author Romain Strock (Twitter: @_srom)
- * @version v1.0.0
+ * @version v1.0.1
  */
 (function($) {
   
